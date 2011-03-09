1b4ae5ec386243aa372c89486a67a14cff308d7f
Igor <igordsm@gmail.com>
2011-03-09 16:55:22 -0300

    CSS on the letters.

--- playhangmanonline/js/game.js
+++ playhangmanonline/js/game.js
@@ -1,8 +1,8 @@
 
 function init() {
 	var links = document.getElementById("alphabet");
-	var wrong = document.getElementById("l").innerText;
-	var buckets = document.getElementById("buckets").innerText;
+	var wrong = document.getElementById("l").innerHTML || "";
+	var buckets = document.getElementById("buckets").innerHTML || "";
 	if (wrong.length > 5) {
 		document.getElementById("loose").style.display = "block";
 		document.getElementById("play_new_game").style.display = "block";
@@ -13,13 +13,17 @@ function init() {
 	var letters = 'abcdefghijklmnopqrstuvxywz';
 	var finalHTML = '';
 	for (var i = 0; i < letters.length; i++) {
-		if (wrong.indexOf(letters[i]) == -1 && buckets.indexOf(letters[i])) {
+		if (wrong.indexOf(letters[i]) == -1 && buckets.indexOf(letters[i]) == -1) {
 			finalHTML += "<a href=\"" + game_link + letters[i] + "\">";
-		}
+		} else {
+            finalHTML += "<span>";
+        }
 		finalHTML += letters[i];
-		if (wrong.indexOf(letters[i]) == -1 && buckets.indexOf(letters[i])) {
+		if (wrong.indexOf(letters[i]) == -1 && buckets.indexOf(letters[i]) == -1) {
 			finalHTML += "</a>";
-		}
+		} else {
+            finalHTML += "</span>";
+        }
 	}	
 	links.innerHTML = finalHTML;
 }
